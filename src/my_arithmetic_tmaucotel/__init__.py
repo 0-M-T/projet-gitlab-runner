164902a2b44a655094382ae4d6ee54da0d1d7c28
tmaucotel <tom.maucotel@etudiant.univ-lr.fr>
2024-11-14 15:41:48 +0100

    simoné

--- src/my_arithmetic_tmaucotel/__init__.py
+++ src/my_arithmetic_tmaucotel/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
 #
 # SPDX-License-Identifier: MIT
+def pgcd(a, b):
+    """Calcule le plus grand commun diviseur (PGCD) de deux entiers a et b."""
+    while b:
+        a, b = b, a % b
+    return a
