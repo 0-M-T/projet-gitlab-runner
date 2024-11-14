7c2601b15b675c278c5dc5f618a06f5f25f81dfa
tmaucotel <tom.maucotel@etudiant.univ-lr.fr>
2024-11-14 15:52:28 +0100

    e

--- src/my_arithmetic/my_arithmetic_tmaucotel/__init__.py
+++ src/my_arithmetic/my_arithmetic_tmaucotel/__init__.py
@@ -1,8 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
 #
 # SPDX-License-Identifier: MIT
-def pgcd(a, b):
-    """Calcule le plus grand commun diviseur (PGCD) de deux entiers a et b."""
+def pgcd(a: int, b: int) -> int:
     while b:
         a, b = b, a % b
     return a
